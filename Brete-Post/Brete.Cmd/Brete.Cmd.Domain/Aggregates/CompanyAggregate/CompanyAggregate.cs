0d6aee8994bcd67d61b7d185d2afd50524f1eeb3
Nebura <hamklieneduprofesional@gmail.com>
2023-11-29 20:55:04 -0600

    Company Aggregate created with method implemented

--- Brete-Post/Brete.Cmd/Brete.Cmd.Domain/Aggregates/CompanyAggregate/CompanyAggregate.cs
+++ Brete-Post/Brete.Cmd/Brete.Cmd.Domain/Aggregates/CompanyAggregate/CompanyAggregate.cs
@@ -1,4 +1,5 @@
-﻿using CQRS.Core.Domain;
+﻿using Brete.Common.Events.Company;
+using CQRS.Core.Domain;
 
 namespace Brete.Cmd.Domain.Aggregates;
 
@@ -6,7 +7,62 @@ public sealed class CompanyAggregate : AggregateRoot
 {
     private bool _active;
     public bool Active { get => _active; set => _active = value; }
-    public CompanyAggregate()
+    public CompanyAggregate(Guid companyId)
     {
+        RaiseEvent(new CompanyCreatedEvent
+        {
+            Id = companyId,
+        });
+    }
+
+    public void Apply(CompanyCreatedEvent @event)
+    {
+        _id = @event.Id;
+        _active = true;
+    }
+
+    public void UpdateCompany(Guid companyId)
+    {
+        RaiseEvent(
+            new CompanyUpdatedEvent
+            {
+                Id = companyId
+            });
+    }
+
+    public void Apply(CompanyUpdatedEvent @event)
+    {
+        _id = @event.Id;
+        _active = true;
+    }
+
+    public void DisableCompany(Guid companyId)
+    {
+        RaiseEvent(
+            new CompanyDisableEvent
+            {
+                Id = companyId
+            });
+    }
+
+    public void Apply(CompanyDisableEvent @event)
+    {
+        _id = @event.Id;
+        _active = true;
+    }
+
+    public void RemoveCompany(Guid companyId)
+    {
+        RaiseEvent(
+            new CompanyRemovedEvent
+            {
+                Id = companyId
+            });
+    }
+
+    public void Apply(CompanyRemovedEvent @event)
+    {
+        _id = @event.Id;
+        _active = true;
     }
 }
